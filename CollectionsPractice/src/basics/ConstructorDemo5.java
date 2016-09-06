b47508b5c527593819169607e40cb0a1eb134ae1
nkasu <nkasu@XHLT0037.xeno.com>
2016-09-06 17:29:27 +0530

    Nag

---
+++ CollectionsPractice/src/basics/ConstructorDemo5.java
@@ -0,0 +1,26 @@
+package basics;
+
+public class ConstructorDemo5 {
+	//instance variables
+	String username,password;
+	
+	void display(){
+		//printing instance variable values
+		System.out.println("User Name is: "+username);
+		System.out.println("Password is: "+password);
+	}
+	
+	ConstructorDemo5(String username, String password){
+		//assigning local values to instance variables
+		this.username=username;
+		this.password=password;
+	}
+	
+	public static void main(String[] args) {
+		ConstructorDemo5 obj = new ConstructorDemo5("nkasu@gmail.com","asdf@123");
+		obj.display();
+		ConstructorDemo5 obj2 = new ConstructorDemo5("vdasari@gmail.com","test1234");
+		obj2.display();
+	}
+
+}
---
+++ CollectionsPractice/src/basics/ConstructorDemo6.java
@@ -0,0 +1,21 @@
+package basics;
+
+public class ConstructorDemo6 {
+	ConstructorDemo6(){
+		this(100);
+		System.out.println("0-arg constructor");
+	}
+	ConstructorDemo6(int a){
+		this("nagarjunreddyk","test@111");
+		System.out.println("1-arg constructor: "+a);
+	}
+	ConstructorDemo6(String username, String password){
+		System.out.println("2-arg constructor: "+username+" --- "+password);
+	}
+
+	public static void main(String[] args) {
+		new ConstructorDemo6();
+
+	}
+
+}
