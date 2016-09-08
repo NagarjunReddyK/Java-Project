3f75b9508208287ee3f211105d18e9312fea051c
nkasu <nkasu@XHLT0037.xeno.com>
2016-09-08 17:20:29 +0530

    Nag

--- CollectionsPractice/src/basics/ConstructorDefault.java
+++ CollectionsPractice/src/basics/ConstructorDefault.java
@@ -11,6 +11,7 @@ public class ConstructorDefault {
 	public static void main(String[] args){
 		ConstructorDefault obj = new ConstructorDefault();
 		obj.login1();
+		//new ConstructorDefault().login1();
 	}
 
 }
