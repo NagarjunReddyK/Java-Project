a3f74577acfef221ac1a002bd99d96acff2702ef
nkasu <nkasu@FHLT0017.xeno.com>
2019-07-25 12:44:21 +0530

    al

---
+++ JavaTraining/src/collections/ArrayListDemo.java
@@ -0,0 +1,74 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		ArrayList<String> al=new ArrayList<String>();
+		al.add("Vendor1");
+		al.add("Vendor4");
+		al.add("Vendor2");//1
+		al.add("Vendor3");
+		al.add("Vendor12");
+		al.add("Vendor10");
+		
+		
+		
+		
+		
+		//al.set(1, "New Vendor");
+		
+		/*System.out.println(al.indexOf("Vendor10"));
+		System.out.println(al);
+		
+		System.out.println(al.get(1));
+		
+		System.out.println(al.size());
+		
+		System.out.println(al.contains("Vendodsfdsfr10"));
+		
+		al.clear();
+		
+		System.out.println(al);*/
+		
+		
+		/*ArrayList<String> al2=new ArrayList<String>();
+		al2.add("Vendor10");
+		al2.add("Vendor20");
+		al2.add("Vendor30");
+		al2.add("Vendor40");
+		
+		
+		System.out.println(al2.removeAll(al));
+		
+		System.out.println(al2);
+		*/
+		
+		/*al.remove("Vendor2");
+		al.remove(0);*/
+		
+		//System.out.println(al);
+		
+		/*al.add(1, "Vendor5");
+		//System.out.println(al);
+		
+		ArrayList<String> al2=new ArrayList<String>();
+		al2.addAll(al);
+		al2.add("Vendor6");
+		System.out.println(al2);
+		
+		al2.add("Vendor6");//0
+		al2.add("Vendor7");//1
+		al2.add("Vendor8");//2
+		
+		al2.addAll(1, al);
+		System.out.println(al2);
+		
+		System.out.println(al2.isEmpty());*/
+		
+		
+		
+		
+	}
+
+}
